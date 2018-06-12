542810770d1febb0bef357e01da69de0203ecc4a
avadgam <adgam@yandex.ru>
2018-06-12 04:19:05 +0400

    Added code that prints the sum of the e-th powers of first n numbers.

--- task1.js
+++ task1.js
@@ -1,3 +1,23 @@
 /* Task 1 */
 
 /* Put your code here */
+const argumentList = process.argv;
+
+let n = Number(argumentList[2]);
+let e = Number(argumentList[3]); 
+
+if (e <= 100 && e >=1 && n>=1 && n<=100) {
+	var sum = 1;
+	var j = 1;
+	for (var i = 2; i<=n; i++) {
+		var q = 1;
+		while (j<=e) {
+			q *= i;
+			j++;
+		}
+		sum += q;			
+	}
+
+	
+	process.stdout.write(`${sum}`);
+} 
