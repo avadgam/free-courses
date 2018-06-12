c4ec9ec9f802b04150d1d05f858dd800db30ecb9
avadgam <adgam@yandex.ru>
2018-06-13 02:13:43 +0400

    Added a function that prints the sum of powers.

--- task1.js
+++ task1.js
@@ -2,22 +2,24 @@
 
 /* Put your code here */
 const argumentList = process.argv;
+const numbers = argumentList.slice(2,argumentList.length);
 
-let n = Number(argumentList[2]);
-let e = Number(argumentList[3]); 
+if (numbers.length === 2) {
+	let n = Number(numbers[0]);
+	let e = Number(numbers[1]); 
+	printSumOfPowers(n, e);
+}
 
-if (e <= 100 && e >=1 && n>=1 && n<=100) {
-	var sum = 1;
-	var j = 1;
-	for (var i = 2; i<=n; i++) {
-		var q = 1;
-		while (j<=e) {
-			q *= i;
-			j++;
-		}
-		sum += q;			
-	}
-
-	
-	process.stdout.write(`${sum}`);
-} 
+function printSumOfPowers(n, e) {
+	if (e <= 100 && e >=1 && n>=1 && n<=100) {
+		var sum = 1;
+		for (var i = 2; i <= n; i++) {
+			var power = 1;
+			for(var j = 1; j <= e;j++) {
+				power *= i;
+			}
+			sum += power;
+		}	
+		process.stdout.write(`${sum}`);
+	} 
+}
